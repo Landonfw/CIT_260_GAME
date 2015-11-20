b915fc1e9944f726933a3f018d0fc0d1fc8602ae
landonwright <landonwright@10.50.72.244>
2015-11-19 20:48:27 -0700

    11/19/2015 - created enum on the character

--- trailOfSpace/src/byui/cit260/trailOfSpace/model/Character.java
+++ trailOfSpace/src/byui/cit260/trailOfSpace/model/Character.java
@@ -11,35 +11,32 @@ import java.util.Objects;
  *
  * @author animejedifreak
  */
-public class Character implements Serializable{
+public enum Character implements Serializable{
     
-    private String type;
-    private String name;
-    private int health;
-    private int attack;
+    Explorer("He is the space explorer."),
+    Alien1("this is one of the enemies you will encounter."),
+    Alien2("This is one of the enemies you will encounter.");
+    
+    private final String type;
+    private final int health;
+    private final int attack;
+    private final String description;
+    private final Point coordinates
     
     
 
-    public Character() {
+    public Character(String description) {
+                this.description = description;
+                coordinates = new Point(1,1);
     }
 
-    
-    
-    
+     
     public String getType() {
         return type;
     }
 
     public void setType(String type) {
         this.type = type;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
     }
 
     public int getHealth() {
@@ -58,50 +55,19 @@ public class Character implements Serializable{
         this.attack = attack;
     }
 
+    public String getDescription() {
+        return description;
+}
+
+    public Point getCoordinates() {
+        return coordinates;
+}
+
     
 
     @Override
     public String toString() {
         return "Character{" + "type=" + type + ", name=" + name + ", health=" + health + ", attack=" + attack + '}';
     }
-
-    @Override
-    public int hashCode() {
-        int hash = 3;
-        hash = 11 * hash + Objects.hashCode(this.type);
-        hash = 11 * hash + Objects.hashCode(this.name);
-        hash = 11 * hash + this.health;
-        hash = 11 * hash + this.attack;
-        
-        return hash;
-    }
-
-    @Override
-    public boolean equals(Object obj) {
-        if (obj == null) {
-            return false;
-        }
-        if (getClass() != obj.getClass()) {
-            return false;
-        }
-        final Character other = (Character) obj;
-        if (!Objects.equals(this.type, other.type)) {
-            return false;
-        }
-        if (!Objects.equals(this.name, other.name)) {
-            return false;
-        }
-        if (this.health != other.health) {
-            return false;
-        }
-        if (this.attack != other.attack) {
-            return false;
-        }
-        
-        return true;
-    }
-    
-    
-    
     
 }
